035e1f3347ff4cc8fed7b76e268a8199cc3ec5cf
David Lee <davidrlee@gmail.com>
2014-12-14 23:05:22 -0500

    Moved index to jade template, added gulp task for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
     path = require('path'),
     less = require('gulp-less'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    jade = require('gulp-jade');
 
 var paths = {
-    styles: ['./app/assets/css/less/app.less']
+    styles: ['./app/assets/css/less/app.less'],
+    index: ['./app/index.jade']
 };
 
 gulp.task('less', function () {
@@ -14,4 +16,18 @@ gulp.task('less', function () {
         }))
         .pipe(minifyCSS({keepBreaks:false}))
         .pipe(gulp.dest('./app/assets/css'));
+});
+
+gulp.task('jade', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src(paths.index)
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./app/'))
+});
+
+gulp.task('build', function(){
+    gulp.run('less', 'jade');
 });
